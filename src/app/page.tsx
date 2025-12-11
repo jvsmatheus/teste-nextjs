80b08078860de396d9b9b2d08096c00f62316a63
jvsmatheus <matheusjvs_@hotmail.com>
2025-12-11 03:53:59 -0300

    Estruturação dos componentes do projeto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,58 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Separator } from "@/components/ui/separator";
-import { ArrowDownRight, Check, List, Plus, SquarePen, Trash } from "lucide-react";
+import {
+  ArrowDownRight,
+  Check,
+  List,
+  ListCheck,
+  Plus,
+  Sigma,
+  SquarePen,
+  Trash,
+} from "lucide-react";
 
 export default function Home() {
   return (
     <main className="w-full h-screen bg-gray-100 flex justify-center items-center">
       <Card className="w-lg">
         <CardHeader className="flex gap-2">
-            <Input placeholder="Adicionar tarefa"></Input>
-            <Button className="cursor-pointer">
-              <Plus />Cadastrar
-            </Button>
+          <Input placeholder="Adicionar tarefa"></Input>
+          <Button className="cursor-pointer">
+            <Plus />
+            Cadastrar
+          </Button>
         </CardHeader>
 
         <CardContent>
           <Separator className="mb-6" />
           <div className="flex gap-2">
-            <Badge className="cursor-pointer" variant={'default'}><List />Todas</Badge>
-            <Badge className="cursor-pointer" variant={'outline'}><ArrowDownRight />Não finalizados</Badge>
-            <Badge className="cursor-pointer" variant={'outline'}><Check />Concluído</Badge>
+            <Badge className="cursor-pointer" variant={"default"}>
+              <List />
+              Todas
+            </Badge>
+            <Badge className="cursor-pointer" variant={"outline"}>
+              <ArrowDownRight />
+              Não finalizados
+            </Badge>
+            <Badge className="cursor-pointer" variant={"outline"}>
+              <Check />
+              Concluído
+            </Badge>
           </div>
 
           <div className="mt-4 border-b">
@@ -29,13 +60,65 @@ export default function Home() {
               <div className="w-1 h-full bg-green-300"></div>
               <p className="flex-1 px-2 text-sm">Estudar react</p>
               <div className="flex gap-2 items-center">
-                <SquarePen size={16} className="cursor-pointer"/>
-                <Trash size={16} className="cursor-pointer"/>
+                
+                <Dialog>
+                  <DialogTrigger><SquarePen size={16} className="cursor-pointer" /></DialogTrigger>
+                  <DialogContent>
+                    <DialogHeader>
+                      <DialogTitle>Editar tarefa</DialogTitle>
+                    </DialogHeader>
+                    <div className="flex gap-2">
+                      <Input placeholder="Editar tarefa"/>
+                      <Button className="cursor-pointer">Editar</Button>
+                    </div>
+                  </DialogContent>
+                </Dialog>
+                <Trash size={16} className="cursor-pointer" />
               </div>
             </div>
           </div>
-        </CardContent>
 
+          <div className="flex justify-between mt-4">
+            <div className="flex gap-2 items-center">
+              <ListCheck size={18} />
+              <p className="text-xs">Tarefas concluidas (3/3)</p>
+            </div>
+
+            <AlertDialog>
+              <AlertDialogTrigger asChild>
+                <Button
+                  className="text-xs h-7 cursor-pointer"
+                  variant={"outline"}
+                >
+                  <Trash /> Limpar tarefas concluidas
+                </Button>
+              </AlertDialogTrigger>
+              <AlertDialogContent>
+                <AlertDialogHeader>
+                  <AlertDialogTitle>
+                    Tem certeza que deseja excluir x itens?
+                  </AlertDialogTitle>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                  <AlertDialogAction>Sim</AlertDialogAction>
+                  <AlertDialogCancel>Não</AlertDialogCancel>
+                </AlertDialogFooter>
+              </AlertDialogContent>
+            </AlertDialog>
+          </div>
+
+          <div className="h-2 w-full bg-gray-100 mt-4 rounded-md">
+            <div
+              className="h-full bg-blue-500 rounded-md"
+              style={{ width: "50%" }}
+            ></div>
+          </div>
+
+          <div className="flex justify-end items-center mt-2 gap-2">
+            <Sigma size={18} />
+            <p className="text-xs">3 tarefas no total</p>
+          </div>
+        </CardContent>
       </Card>
     </main>
   );
